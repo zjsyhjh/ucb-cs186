6c6448ff5df59df4d4f50d529ad377c77564ce66
Jiahuan <zjsyhjh@gmail.com>
2016-04-20 18:38:30 +0800

    modify JoinPredicate.java

--- CS186-proj2/src/java/simpledb/JoinPredicate.java
+++ CS186-proj2/src/java/simpledb/JoinPredicate.java
@@ -1,6 +1,7 @@
 package simpledb;
 
 import java.io.Serializable;
+import javax.sql.rowset.Predicate;
 
 /**
  * JoinPredicate compares fields of two tuples using a predicate. JoinPredicate
@@ -12,7 +13,7 @@ public class JoinPredicate implements Serializable {
 
     /**
      * Constructor -- create a new predicate over two fields of two tuples.
-     * 
+     *
      * @param field1
      *            The field index into the first tuple in the predicate
      * @param field2
@@ -24,36 +25,59 @@ public class JoinPredicate implements Serializable {
      *            Predicate.Op.LESS_THAN_OR_EQ
      * @see Predicate
      */
+
+    /* my code for JoinPredicate */
+    private int field1;
+    private int field2;
+    private Predicate.Op op;
+    /* my code for JoinPredicate */
     public JoinPredicate(int field1, Predicate.Op op, int field2) {
         // some code goes here
+        /* my code for JoinPredicate */
+        this.field1 = field1;
+        this.field2 = field2;
+        this.op = op;
+        /* my code for JoinPredicate */
     }
 
     /**
      * Apply the predicate to the two specified tuples. The comparison can be
      * made through Field's compare method.
-     * 
+     *
      * @return true if the tuples satisfy the predicate.
      */
     public boolean filter(Tuple t1, Tuple t2) {
         // some code goes here
-        return false;
+        //return false;
+        /* my code for JoinPredicate */
+        return t1.getField(field1).compare(op, t2.getField(field2));
+        /* my code for JoinPredicate */
     }
-    
+
     public int getField1()
     {
         // some code goes here
-        return -1;
+        //return -1;
+        /* my code for JoinPredicate */
+        return field1;
+        /* my code for JoinPredicate */
     }
-    
+
     public int getField2()
     {
         // some code goes here
-        return -1;
+        //return -1;
+        /* my code for JoinPredicate */
+        return field2;
+        /* my code for JoinPredicate */
     }
-    
+
     public Predicate.Op getOperator()
     {
         // some code goes here
-        return null;
+        //return null;
+        /* my code for JoinPredicate */
+        return op;
+        /* my code for JoinPredicate */
     }
 }
